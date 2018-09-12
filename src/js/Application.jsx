25895b4f11982e45924c9472601c53e724d420cd
adiyathereh <ad.kydy@gmail.com>
2018-09-12 16:33:47 +0200

    first commit

---
+++ src/js/Application.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Application extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Let's get started with React!</h1>
+            </div>
+        )
+    }
+}
+
+export default Application
